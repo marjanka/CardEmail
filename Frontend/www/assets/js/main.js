8772e41d468a857eef45027d7cdc6f92d79eefb0
Marianna <manja6669@gmail.com>
2016-12-14 09:58:19 +0200

    Add elements to main page and try to do the function of sending messafge to email

--- Frontend/www/assets/js/main.js
+++ Frontend/www/assets/js/main.js
@@ -138,10 +138,7 @@ var getCanvas; // global variable
 $('.left-panel input,textarea').each(function(){
 
     $(this).bind('blur keypress', function() {
-
-        if($(this).attr('id') == 'img') $('#new_img').attr('src',$(this).val());
-        else $('#new_' + $(this).attr('id')).text($(this).val());
-
+ $('#new_' + $(this).attr('id')).text($(this).val());
     });
 });
 
@@ -159,16 +156,65 @@ $(".preview").on('click', function () {
     $(".card-preview").show();
 
 });
-
-
+var imageData1;
 $(".download").on('click', function () {
-    var imageData = getCanvas.toDataURL("image/jpg");
+    var imageData = getCanvas.toDataURL("image/png");
+    console.log(imageData);
+
     // Now browser starts downloading it instead of just showing it
-    var newData = imageData.replace(/^data:image\/jpg/, "data:application/octet-stream");
-    $(".download").attr("download", "greeting_card.jpg").attr("href", newData);
+    var newData = imageData.replace(/^data:image\/png/, "data:application/octet-stream");
+    $(".download").attr("download", "greeting_card.png").attr("href", newData);
 });
 
 
+$(".send-email").on('click', function () {
+  //  alert("bla");
+    var imageData = getCanvas.toDataURL("image/png");
+    console.log(imageData);
+
+ // $(".send_image").value = imageData;
+   // document.getElementById("send_image").innerHTML = imageData;
+   // if (!getElementById("send_image")){
+   //     alert("loh");
+  //  }
+   // getElementById("send_image").value = imageData;
+   // var bla = getElementById("send_image").value;
+  //  alert("bla");
+
+
+//var element1 = $(".send_image").val();
+//console.log(element1 );
+alert("bla");
+var myform = $("form#myform");
+var $myform = $(myform);
+    $myform.find("input#send_image").val(imageData);
+   // alert($myform.find("input#send_image").text(imageData));
+    myform.submit(function(event){
+    event.preventDefault();
+
+    var params = myform.serializeArray().reduce(function(obj, item) {
+        obj[item.name] = item.value;
+        return obj;
+        console.log(obj);
+    }, {});
+
+    // Change to your service ID, or keep using the default service
+    var service_id = "default_service";
+
+    var template_id = "cardemail";
+    myform.find("button").text("Sending...");
+    emailjs.send(service_id,template_id,params)
+        .then(function(){
+            alert("Email send!");
+            window.location.replace('card.html');
+        }, function(err) {
+            alert("Send email failed!\r\n You write a wrong name or email address.\n Try again ");
+            myform.find("button").text("Send");
+        });
+    return false;
+});
+
+});
 },{"../templates":7,"./storage":6}],4:[function(require,module,exports){
 /**
  * Created by Marjana on 12/11/2016.
